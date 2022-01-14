fe25368021f37af6a875aee6091c2661c1a13bb8
Simon Masinde <smasinde@gmail.com>
2022-01-14 13:46:03 +0300

    changes

--- src/main.ts
+++ src/main.ts
@@ -4,72 +4,94 @@ const app = document.querySelector<HTMLDivElement>('#app')!
 
 const header = document.createElement("header");
 const logo = document.createElement("h1");
-const canvas = document.createElement("canvas");
-const ctx = canvas.getContext("2d");
-
-canvas.id = "canvas1";
+let canvas: HTMLCanvasElement;
+let ctx: CanvasRenderingContext2D;
 
 logo.textContent = "Aesthetics";
 header.appendChild(logo);
 
 app.appendChild(header);
-app.appendChild(canvas);
 
 window.onload = function () {
+    canvas = document.createElement("canvas");
+    canvas.id = "canvas1";
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight / 2;
 
+    const ratio = 0.80;
+    let fontSize = canvas.height * ratio;
+
+    ctx = canvas.getContext("2d")!;
+
+    app.appendChild(canvas);
+
     if (ctx !== null) {
-        ctx.font = "250px Ariel";
-        ctx.fillStyle = "#fff";
-        ctx.fillText("Aesthetics", 0, 250);
-        let imgData: ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        let data: Uint8ClampedArray = imgData.data;
-        console.log(data);
-        
+        ctx.font = (fontSize | 0) + "px serif";
+        const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const textWidth = ctx.measureText("Aesthetics").width;
+        let linesArray: Array<Contrastline> = [];
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "center";
         const startTime = performance.now();
-        // new ContrastLine(ctx, 90, 20);
-        for(let y = 0; y <= canvas.height; y++){
-            console.log(data[y])
-            for(let x = 0; x <= canvas.width; x+=4){
-                console.log(data[x]);
+        let mappedImage = [];
+        
+        for (let y = 0; y < canvas.height; y++) {
+            let row = [];
+            for (let x = 0; x < canvas.width; x++) {
+                const black = pixels.data[(y * 4 * pixels.width) + (x * 4)];
             }
         }
+        class Contrastline {
+            x: number;
+            y: number;
+            size: number;
+
+            constructor() {
+                this.x = Math.random() * canvas.width;
+                this.y = 0;
+                this.size = Math.random() * 1.5 + 1
+            }
+
+            update() {
+                this.y += Math.random() * 3.5;
+                if (this.y >= canvas.height) {
+                    this.y = 0;
+                    this.x = Math.random() * canvas.width;
+                }
+            }
+
+            draw() {
+                ctx.beginPath();
+                ctx.fillStyle = 'white';
+                ctx.strokeStyle = 'white';
+                ctx.moveTo(this.x, this.y);
+                ctx.lineTo(this.x + 50, this.y);
+                ctx.stroke();
+            }
+        }
+        async function init() {
+            for (let i = 0; i < 500; i++) {
+                linesArray.push(new Contrastline());
+            }
+        }
+        init();
+        async function animate() {
+            ctx.fillStyle = 'rgba(0, 0, 0, 0.1)'
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            ctx.fillStyle = "#fff";
+            ctx.fillText("Aesthetics", canvas.width / 2, canvas.height / 2);
+            for (let i = 0; i < linesArray.length; i++) {
+                linesArray[i].update();
+                linesArray[i].draw();
+            }
+            requestAnimationFrame(animate);
+        }
+        animate();
         const endTime = performance.now();
 
         // ctx.putImageData(ctx.getImageData(0, 0, 100, 5), 0, 150,);
         // ctx.putImageData(ctx.getImageData(0, 0, 100, 5), 0, 200,);
         console.log(startTime, endTime);
     };
-}
-window.onresize = function () {
-    canvas.width = window.innerWidth;
-
-    if (ctx !== null) {
-        ctx.font = "250px Ariel";
-        ctx.fillStyle = "#fff";
-        ctx.fillText("Aesthetics", 0, 250);
-    };
-
-}
-
-class ContrastLine {
-    #ctx;
-    #startX;
-    #startY;
-    constructor(ctx: CanvasRenderingContext2D, startX: number, startY: number) {
-        this.#ctx = ctx;
-        this.#startX = startX;
-        this.#startY = startY;
-        this.#ctx.strokeStyle = "white";
-        this.#ctx.lineWidth = 2.0;
-        this.#draw(this.#startX, this.#startY);
-    }
-
-    #draw(x: number, y: number) {
-        this.#ctx.beginPath();
-        this.#ctx.moveTo(x, y);
-        this.#ctx.lineTo(x + 100, y);
-        this.#ctx.stroke();
-    }
 }
